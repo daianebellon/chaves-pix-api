f27925f36210a0149cbac83cd2b765fb38da74c3
daiane.bellon <daianebellon@hotmail.com>
2021-08-03 14:45:04 -0300

    import inicial

---
+++ src/main/java/com/daiane/pix/ChavesPixApiApplication.java
@@ -0,0 +1,13 @@
+package com.daiane.pix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChavesPixApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChavesPixApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daiane/pix/ChavesPixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daiane.pix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChavesPixApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
